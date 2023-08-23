47ae6ff4100e3b1394fea91ff6644df297ff99fa
Bogdan <bogd.moskv@gmail.com>
2023-08-23 11:48:00 +0300

    Initial commit

---
+++ Inforce .NET Task_Moskvichev Bogdan/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Inforce_.NET_Task_Moskvichev_Bogdan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Inforce_.NET_Task_Moskvichev_Bogdan.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Inforce .NET Task_Moskvichev Bogdan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inforce_.NET_Task_Moskvichev_Bogdan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
