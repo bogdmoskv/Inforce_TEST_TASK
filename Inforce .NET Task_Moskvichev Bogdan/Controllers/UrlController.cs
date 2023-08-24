cbd1d8c89a90da3af333b6dc96a4b87ca4523b8c
Bogdan <bogd.moskv@gmail.com>
2023-08-24 21:56:10 +0300

    Refactoring

--- Inforce .NET Task_Moskvichev Bogdan/Controllers/UrlController.cs
+++ Inforce .NET Task_Moskvichev Bogdan/Controllers/UrlController.cs
@@ -20,76 +20,89 @@ namespace Inforce_.NET_Task_Moskvichev_Bogdan.Controllers
             _context = context;
         }
 
+        private async Task<User?> GetUserFromToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+            if (jwtToken == null)
+            {
+                return null;
+            }
+
+            var claims = jwtToken.Claims;
+
+            var userEmailClaim = claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+            if (userEmailClaim == null)
+            {
+                return null;
+            }
+
+            var userEmail = userEmailClaim.Value;
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == userEmail);
+            return user;
+        }
+
+
 
         [HttpPost("shorturl")]
-        public async Task<IActionResult> CheckUrl(UrlDto url)
+        public async Task<IActionResult> AddNewUrl(UrlDto url)
         {
             string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Replace("Bearer ", "");
 
             if (string.IsNullOrEmpty(token))
             {
-                return Unauthorized(); 
+                return Unauthorized();
             }
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
-            if (jwtToken == null)
-            {
-                return Unauthorized(); 
-            }
+            var user = await GetUserFromToken(token);
 
-            var claims = jwtToken.Claims;
-
-
-            var userEmailClaim = claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
-            if (userEmailClaim == null)
-            {
-                return Unauthorized(); 
-            }
-
-            var userEmail = userEmailClaim.Value;
-
-
-            var user = _context.Users.FirstOrDefault(u => u.Email == userEmail);
             if (user == null)
             {
                 return Unauthorized();
             }
 
-
             if (!Uri.TryCreate(url.Url, UriKind.Absolute, out var inputUrl))
-                return BadRequest("Invalid url has been provided!");
-
-            var existingUrl = await _context.Urls.FirstOrDefaultAsync(u => u.Url == url.Url);
-            if (existingUrl != null)
             {
-                return BadRequest("This URL already exists in the database.");
+                return BadRequest("Invalid url has been provided!");
             }
 
-
-            Random random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@az";
-            var randomStr = new string(Enumerable.Repeat(chars, 8)
-                 .Select(x => x[random.Next(x.Length)]).ToArray());
-
-            var sUrl = new UrlManagement()
+            try
             {
-                Url = url.Url,
-                ShortUrl = randomStr,
-                OwnerId=user.Id,
-            };
-          
-            _context.Urls.Add(sUrl);
-            await _context.SaveChangesAsync();
+                var existingUrl = await _context.Urls.FirstOrDefaultAsync(u => u.Url == url.Url);
+                if (existingUrl != null)
+                {
+                    return BadRequest("This URL already exists in the database.");
+                }
 
-            var httpContext = ControllerContext.HttpContext;
-            var result = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/{sUrl.ShortUrl}";
-            sUrl.ShortUrl = result;
-           
-            return Ok(sUrl);
+                Random random = new Random();
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@az";
+                var randomStr = new string(Enumerable.Repeat(chars, 8)
+                    .Select(x => x[random.Next(x.Length)]).ToArray());
 
-          
+                var sUrl = new UrlManagement()
+                {
+                    Url = url.Url,
+                    ShortUrl = randomStr,
+                    OwnerId = user.Id,
+                };
+
+                _context.Urls.Add(sUrl);
+                await _context.SaveChangesAsync();
+
+                var httpContext = ControllerContext.HttpContext;
+                var result = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/{sUrl.ShortUrl}";
+                sUrl.ShortUrl = result;
+
+                return Ok(sUrl);
+            }
+            catch
+            {
+                return BadRequest("An error occurred while processing the request");
+            }
         }
+
+
 
         [HttpGet("getAll")]
         public async Task<IActionResult> GetAllUrls()
